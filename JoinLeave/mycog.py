0a47bf96679f7753dc632d2a1ace471a32b880d7
Yuuki <singhrobin2628@gmail.com>
2021-05-28 08:44:47 +0200

    avatar fix test

--- JoinLeave/mycog.py
+++ JoinLeave/mycog.py
@@ -9,7 +9,7 @@ class JoinLeave(commands.Cog):
         channel = discord.utils.get(member.guild.channels, name="gate-in-out")
         #message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         embed = discord.Embed(title="A new member joined")
-        embed.set_thumbnail(member.avatar_url)
+        embed.set_thumbnail(f"https://cdn.discordapp.com/avatars/{member.id}/{member.avatar}.png?size=1024".format(member))
         embed.add_field(name="**Member**", value=member.mention, inline=False)
         embed.add_field(name="**Name**", value="{member.name}#{member.discriminator}", inline=False)
 
@@ -20,7 +20,7 @@ class JoinLeave(commands.Cog):
         channel = discord.utils.get(member.guild.channels, name="gate-in-out")
         #message = f"{member.mention} has left the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         embed = discord.Embed(title="A member left")
-        embed.set_thumbnail(member.avatar_url)
+        embed.set_thumbnail(f"https://cdn.discordapp.com/avatars/{member.id}/{member.avatar}.png?size=1024".format(member))
         embed.add_field(name="**Member**", value=member.mention, inline=False)
         embed.add_field(name="**Name**", value="{member.name}#{member.discriminator}", inline=False)
 
