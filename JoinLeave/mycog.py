b46e58f68399f448a3aa706bc290ea2f630bdbed
Yuuki <singhrobin2628@gmail.com>
2021-05-28 08:13:08 +0200

    update

--- JoinLeave/mycog.py
+++ JoinLeave/mycog.py
@@ -4,13 +4,16 @@ import discord
 class JoinLeave(commands.Cog):
     """A cog to notify when someone joins or leaves the server"""
 
-    @client.event
+    def listener(name=None):
+        return lambda x:x
+
+    @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = 374597911436328971
         message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         await channel.send(message)
 
-    @client.event
+    @commands.Cog.listener()
     async def on_member_remove(self, member):
         channel = 374597911436328971
         message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
