d48b2d9bba4ef6359f3f8ec3c054a1ca06513c93
Yuuki <singhrobin2628@gmail.com>
2021-05-28 08:26:35 +0200

    final test

--- JoinLeave/mycog.py
+++ JoinLeave/mycog.py
@@ -13,6 +13,6 @@ class JoinLeave(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         channel = discord.utils.get(member.guild.channels, name="gate-in-out")
-        message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
+        message = f"{member.mention} has left the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         await channel.send(message)
         
