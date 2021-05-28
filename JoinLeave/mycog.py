e24e98a1786b50d644c52e174fb07dbf01d879a9
Yuuki <singhrobin2628@gmail.com>
2021-05-28 09:58:41 +0200

    channel check

--- JoinLeave/mycog.py
+++ JoinLeave/mycog.py
@@ -9,7 +9,6 @@ class JoinLeave(commands.Cog):
         channel = discord.utils.get(member.guild.channels, name="gate-in-out")
         if(channel in member.guild.channels):
             return
-        #message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         embed = discord.Embed(title="A member joined", color = 0x2ECC71)
         embed.set_thumbnail(url=member.avatar_url)
         embed.add_field(name="**Member**", value=member.mention, inline=False)
@@ -20,7 +19,8 @@ class JoinLeave(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         channel = discord.utils.get(member.guild.channels, name="gate-in-out")
-        #message = f"{member.mention} has left the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
+        if(channel in member.guild.channels):
+            return
         embed = discord.Embed(title="A member left", color = 0xff0000)
         embed.set_thumbnail(url=member.avatar_url)
         embed.add_field(name="**Member**", value=member.mention, inline=False)
