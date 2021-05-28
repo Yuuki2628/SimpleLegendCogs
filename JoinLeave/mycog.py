052f588c77a003c62df0da152c7bbb054af97b8c
Yuuki <singhrobin2628@gmail.com>
2021-05-28 08:14:01 +0200

    test

--- JoinLeave/mycog.py
+++ JoinLeave/mycog.py
@@ -9,13 +9,13 @@ class JoinLeave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = 374597911436328971
+        channel = 746001104336322611
         message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         await channel.send(message)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        channel = 374597911436328971
+        channel = 746001104336322611
         message = f"{member.mention} has joined the server\nUser id: {member.id}\nCurrent name: {member.name}#{member.discriminator}"
         await channel.send(message)
         
