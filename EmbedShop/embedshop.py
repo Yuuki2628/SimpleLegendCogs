fb4935321365c4ac3d1d379abf1433d619318f11
Yuuki <singhrobin2628@gmail.com>
2021-06-02 15:23:01 +0200

    shop balancing

--- EmbedShop/embedshop.py
+++ EmbedShop/embedshop.py
@@ -4,37 +4,43 @@ import discord
 class EmbedShop(commands.Cog):
     """Embeds for the Elite Embed Updates"""
 
+#Billionaire: 815958379624529931
+#Elite: 815958696982872075
+#Legendary: 815958673770807317
+#Epic: 815958568418803772
+#Rare: 815957842266816522
+
     @commands.command(name="es_shop")
     async def es_shop(self, ctx):
         if(ctx.author.id != 295275466703503372):
             return await ctx.send("Only the true bot director shall be allowed to do this")
         embed = discord.Embed(title="LeGeND Shop", description="Note: to purchase any item that doesn't have a command specified, dm <@598662722821029888>")
-        embed.add_field(name="__Custom background__", value="Change the picture of your !profile background.\nCost: 800,000 credits", inline=False)
-        embed.add_field(name="__Emoji Flair__", value="Add/Change an emoji in your nickname. No custom emojis allowed.\nCost: 200,000 credits", inline=False)
-        embed.add_field(name="__Custom Command__", value="Have LeGeND Bot say a line of text or perform a command when people type !<a custom word>.\nCost: 500,000 credits", inline=False)
-        embed.add_field(name="__Rare__", value="Appear on the top of the members list with an orange color name. Receive more credits in payday.\nCost: 1,500,000 credits.\nCommand: `!buyrare`", inline=False)
-        embed.add_field(name="__Epic__", value="Appear higher than <@&815957842266816522> and with color Purple. Receive more credits in payday.\nCost: 3,500,000 credits.\nCommand: `!buyepic`", inline=False)
-        embed.add_field(name="__Legendary__", value="Show you are a true LeGeND and appear higher than <@&815958568418803772> with a private LeGeND Channel. Receive more credits in payday.\nCost: 10,000,000 credits.\nCommand: `!buylegendary`", inline=False)
-        embed.add_field(name="__Elite__", value="Join the most outstanding and limited club and appear higher than <@&815958673770807317> with a private LeGeND Channel.\nCost: 35,000,000 credits.\nCommand: `!buyelite`", inline=False)
-        embed.add_field(name="__Custom Adventure Boss__", value="Ever wanted to fight yourself or some creature of your choise in adventure? It's your lucky day because now you can challenge anything with the power of this item.\nCost: 8,000,000 credits.\n<@&815958696982872075> or <@&815958379624529931> required.", inline=False)
-        embed.add_field(name="__Set loot chest__", value="Are you a sets collector? Or are you simply looking for a new set to use in battle? You can buy a set chest from here.\nCost: 1,000,000 credits.\n<@&815958696982872075> or <@&815958379624529931> required.", inline=False)
+        embed.add_field(name="__Custom background__", value="Change the picture of your `!profile` background.", inline=False)
+        embed.add_field(name="__Emoji Flair__", value="Add/Change an emoji in your nickname. No custom emojis allowed.", inline=False)
+        embed.add_field(name="__Custom Command__", value="Have LeGeND Bot say a line of text or perform a command when people type !<a custom word>.", inline=False)
+        embed.add_field(name="__Rare__", value="Appear on the top of the members list with an orange color name. Receive more credits in payday.", inline=False)
+        embed.add_field(name="__Epic__", value="Appear higher than <@&815957842266816522> and with color Purple. Receive more credits in payday.", inline=False)
+        embed.add_field(name="__Legendary__", value="Show you are a true LeGeND and appear higher than <@&815958568418803772> with a private LeGeND Channel. Receive more credits in payday.", inline=False)
+        embed.add_field(name="__Elite__", value="Join the most outstanding and limited club and appear higher than <@&815958673770807317> with a private LeGeND Channel.", inline=False)
+        embed.add_field(name="__Custom Adventure Boss__", value="Ever wanted to fight yourself or some creature of your choise in adventure? It's your lucky day because now you can challenge anything with the power of this item.", inline=False)
+        embed.add_field(name="__Set loot chest__", value="Are you a sets collector? Or are you simply looking for a new set to use in battle? You can buy 10 set chests at a time.", inline=False)
         embed.add_field(name="**__NEW EMBED UPDATE__**", value="Do you want to make your custom commands look unique and special? Look no further, we (I) are proud to introduce the Embed update.\nThere are 2 different embed updates available for purchase:", inline=False)
-        embed.add_field(name="__Simple Embed update__", value="Your command turns into a simple embed (no links or pictures)\nCost: 5,000,000.\n<@&815958673770807317> or <@&815958379624529931> required.", inline=False)
-        embed.add_field(name="__Elite Embed update__", value="The simple update isn't enought for you? You can add links, picture and other cool stuff to you embed with this powerful update.\nThe Elite Embed update lets you personalize your embed command almost as you wish.\n||The result will be similar to the shop embed.||\nCost: 15,000,000.\n<@&815958696982872075> or <@&815958379624529931> required.", inline=False)
-        await ctx.send(embed=embed)
-        
-    @commands.command(name="es_tshop")
-    async def es_tshop(self, ctx):
-        if(ctx.author.id != 295275466703503372):
-            return await ctx.send("Only the true bot director shall be allowed to do this")
-        embed = discord.Embed(title="LeGeND Shop", description="Note: to purchase any item that doesn't have a command specified, dm <@598662722821029888>")
-        embed.add_field(name="__Custom background__", value="Change the picture of your !profile background.\nCost: 800,000 credits", inline=False)
-        embed.add_field(name="__Emoji Flair__", value="Add/Change an emoji in your nickname. No custom emojis allowed.\nCost: 200,000 credits", inline=False)
-        embed.add_field(name="__Custom Command__", value="Have LeGeND Bot say a line of text or perform a command when people type !<a custom word>.\nCost: 500,000 credits", inline=False)
-        embed.add_field(name="__Rare__", value="Appear on the top of the members list with an orange color name. Receive more credits in payday.\nCost: 1,500,000 credits.\nCommand: `!buyrare`", inline=False)
-        embed.add_field(name="__Epic__", value="Appear higher than <@&815957842266816522> and with color Purple. Receive more credits in payday.\nCost: 3,500,000 credits.\nCommand: `!buyepic`", inline=False)
-        embed.add_field(name="__Legendary__", value="Show you are a true LeGeND and appear higher than <@&815958568418803772> with a private LeGeND Channel. Receive more credits in payday.\nCost: 10,000,000 credits.\nCommand: `!buylegendary`", inline=False)
-        embed.add_field(name="__Elite__", value="Join the most outstanding and limited club and appear higher than <@&815958673770807317> with a private LeGeND Channel.\nCost: 35,000,000 credits.\nCommand: `!buyelite`", inline=False)
+        embed.add_field(name="__Simple Embed update__", value="Your command turns into a simple embed (no links or pictures).", inline=False)
+        embed.add_field(name="__Elite Embed update__", value="The simple update isn't enought for you? You can add links, picture and other cool stuff to you embed with this powerful update.\nThe Elite Embed update lets you personalize your embed command almost as you wish.\n||The result will be similar to the shop, the roles or the rules embed.||", inline=False)
+        embed.add_field(name="**Items prices**", value="```\n"
+            "Item name                   Price     Elite price        Command"
+            "Custom background:        800,000\n"
+            "Emoji flair:              200,000\n"
+            "Custom command:           500,000\n"
+            "Rare:                   1,500,000                        !buyrare\n"
+            "Epic:                   3,500,000                        !buyepic\n"
+            "Legendary:             10,000,000                        !buylegendary\n"
+            "Elite:                 35,000,000                        !buyelite\n"
+            "Custom adventure boss: 10,000,000       5,000,000\n"
+            "x10 set loot chests:    5,000,000       2,500,000\n"
+            "Simple embed update:    5,000,000       2,500,000\n"
+            "Elite embed update:    15,000,000      15,000,000\n"
+            "```", inline=False)
         await ctx.send(embed=embed)
         
     @commands.command(name="es_yuuki")
