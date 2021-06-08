036230db6f003ba47c3dc8fe09dc39bd50302ee8
Yuuki <singhrobin2628@gmail.com>
2021-06-08 11:03:47 +0200

    updated the join listener

--- JoinLeave/joinleave.py
+++ JoinLeave/joinleave.py
@@ -85,7 +85,7 @@ class JoinLeave(commands.Cog):
 
                     dm_channel = await member.create_dm()
                     await dm_channel.send(f"You tried joining the server, but I found that your name contains something that might be self advertisement\nPlease remove `{word}` from your name and try joining again")
-                    await kick(member)
+                    await myguild.kick(member, reason=f"Username contained: {word}, possible spam")
                     return await channel.send(embed=embed)
 
         if(channel in member.guild.channels):
