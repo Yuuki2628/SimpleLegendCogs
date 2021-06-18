39e07080035495f5260b83e4de3eee6feb2ae2c1
Yuuki <singhrobin2628@gmail.com>
2021-06-18 12:47:17 +0200

    fixed a bug

--- russian/russianroulette.py
+++ russian/russianroulette.py
@@ -49,7 +49,7 @@ class RussianRoulette(commands.Cog):
         settings = await self.config.guild(ctx.guild).all()
         if(bid is 0):
             bid = settings["MinCost"]
-            settings.Cost.set(bid)
+            settings["Cost"].set(bid)
         if await self.game_checks(ctx, settings, bid):
             await self.add_player(ctx, settings, bid)
 
@@ -116,10 +116,10 @@ class RussianRoulette(commands.Cog):
             num_players = len(players)
 
         if(num_players == 1):
-            if settings.Cost() > settings.MinCost():
-                settings.Cost.set(bid)
+            if settings["Cost"] > settings["MinCost"]:
+                settings["Cost"].set(bid)
             else:
-                settings.Cost.set(settings.MinCost())
+                settings["Cost"].set(settings["MinCost"])
 
         try:
             await bank.withdraw_credits(ctx.author, settings["Cost"])
